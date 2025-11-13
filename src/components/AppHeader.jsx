849571f135dfc3b9a6e406fde201bea86f659054
F4cess662 <suttipongsumrit@gmail.com>
2025-11-13 23:09:51 +0700

    delete-source code

--- src/components/AppHeader.jsx
+++
@@ -1,9 +0,0 @@
-const AppHeader = () => {
-  return (
-    <div className="bg-success fs-1 text-center mb-3 text-white p-3">
-      CSI-205(L) การพัฒนาโปรแกรมส่วนหน้า
-    </div>
-  );
-};
-
-export default AppHeader;
